98979f28a36052ca67e01b67cd15c2f51ff72992
Jonathan.Hagerman <Jonathan.Hagerman@concentrix.com>
2016-12-30 20:25:18 -0800

    Command line argument handling
    Refactored the code to use functions.
    
    Added logic to handle options for displaying the usage information.

--- palindromes/python/palindrome.py
+++ palindromes/python/palindrome.py
@@ -2,15 +2,47 @@
 import sys
 import re
 
+def isPalindrome(line):
+    p = re.compile('\W')
+
+    lineLower = line.lower()
+    lineRegEx = p.sub('', lineLower)
+    lineReverse = lineRegEx[::-1]
+
+    print(lineRegEx == lineReverse)
+
+    return    
+
+def usage():
+    print("Usage: palindrome \"Test String\"")
+    return    
+
+def isOption(arg):
+    return {
+        "?" : True,
+        "-?" : True,
+        "-h" : True,
+        "-H" : True,
+        "/?" : True,
+        "/h" : True,
+        "/H" : True,
+        "--help" : True
+        }.get(arg, False)
+
+
+if len(sys.argv) != 2:
+    usage()
+    exit()
+
 script, line = sys.argv
 
-lineLower = line.lower()
+if isOption(line):
+    usage()
+    exit()
 
-p = re.compile('\W')
+isPalindrome(line)
+exit()
 
-lineRegEx = p.sub('', lineLower)
 
-lineReverse = lineRegEx[::-1]
 
-print(lineRegEx == lineReverse)
 
